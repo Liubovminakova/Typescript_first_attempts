4209a4d340135a139215c88eee4263d6dfa8bc75
Liubovminakova <msnakova7@gmail.com>
2021-12-13 15:49:40 +0100

    first app using TS

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const AboutPage: React.FC = () => {
+  return(
+    <>
+    <h1>Info page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nostrum
+        vero, repudiandae expedita beatae iure? Mollitia tempore quae qui
+        voluptatum.
+    </p>
+    </>
+  )
+}
